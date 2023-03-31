b7b323cfd695114bf156f1ea7a0f21ba0c0f555b
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-03-31 11:38:06 +0200

    add buttons

--- src/components/ControlPanel.vue
+++ src/components/ControlPanel.vue
@@ -1,20 +1,35 @@
 <script>
 //import { ref } from 'vue';
-import { Col, Row } from 'vant';
+import { Col, Row, Button } from 'vant';
 
 export default {
   components: {
     Row,
     Column: Col,
+    Btn: Button,
   },
   setup() {},
 };
 </script>
 
 <template>
-  <Row>
-    <Column span="8">1</Column>
-    <Column span="8">2</Column>
-    <Column span="8">3</Column>
-  </Row>
+  <div class="ControlPanel">
+    <Row gutter="8">
+      <Column span="8">
+        <Btn icon="stop-circle-o" size="large">Stop</Btn>
+      </Column>
+      <Column span="8">
+        <Btn icon="play-circle-o" type="primary" size="large">Start</Btn>
+      </Column>
+      <Column span="8">
+        <Btn icon="pause-circle-o" type="primary" size="large">Pause</Btn>
+      </Column>
+    </Row>
+  </div>
 </template>
+
+<style lang="postcss">
+.ControlPanel {
+  padding: 8px;
+}
+</style>
