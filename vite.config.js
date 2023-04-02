980d4c7ad0b66689018e8db7824fd97a5675005f
Dmitry Kuznetsov <dmitry.kuznetsov.it@gmail.com>
2023-04-02 14:31:26 +0200

    make pwa work

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,39 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Workout Progress Bar',
+        short_name: 'WPB',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#1e293b',
+        description: 'Workout Progress Bar',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
